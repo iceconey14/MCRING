0a7891cfb055d6ae287ec8ae0d60f753f15941ed
iceconey14 <147879255+iceconey14@users.noreply.github.com>
2024-03-04 14:16:33 -0800

    Update onionring-variables.js

--- onionring-variables.js
+++ onionring-variables.js
@@ -1,6 +1,6 @@
 // onionring.js is made up of four files - onionring-widget.js, onionring-index.js, onionring-variables.js (this one!), and onionring.css
 // it's licensed under the cooperative non-violent license (CNPL) v4+ (https://thufie.lain.haus/NPL.html)
-// it was originally made by joey + mord of allium (蒜) house, last updated 2020-11-24
+// it was originally made by joey + mord of allium (è’œ) house, last updated 2020-11-24
 
 // === ONIONRING-VARIABLES ===
 //this file contains the stuff you edit to set up your specific webring
@@ -8,22 +8,21 @@
 //the full URLs of all the sites in the ring
 var sites = [
 'https://oerrorpage.neocities.org',
- 'https://iceconey14.github.io/retro-webring'
-
+'https://iceconey14.github.io/'
 ];
 
 //the name of the ring
-var ringName = 'retroring';
+var ringName = 'retro-webring';
 
 /* the unique ID of the widget. two things to note:
  1) make sure there are no spaces in it - use dashes or underscores if you must
  2) remember to change 'webringid' in the widget code you give out and all instances of '#webringid' in the css file to match this value!*/
-var ringID = 'retro-webring';
+var ringID = 'r-wr';
 
 //should the widget include a link to an index page?
 var useIndex = false;
 //the full URL of the index page. if you're not using one, you don't have to specify anything here
-var indexPage = 'https://iceconey14.github.io/retro-webring/index.html';
+var indexPage = 'https://iceconey14.github.io/retrowebring/index.html';
 
 //should the widget include a random button?
-var useRandom = false;
+var useRandom = true;
